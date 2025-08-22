f17cbc99811c9930931f7756fff6f5679b595ed9
Jaider bautista Rodriguez <jaiderbautista321@gmail.com>
2025-08-21 22:17:10 -0500

    Add homepage, about, contact sections and style updates
    Introduced new Home, About, and Contact sections in index.html, including skill cards and social links. Updated styles.css with enhanced layout, colors, and animations for improved UI/UX. Added social and profile images. Improved menu navigation and button logic in script.js for better interactivity.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,33 @@
 const btn = document.getElementById("menuBtn");
+const btn2 = document.getElementById("menuId1");
+const btn3 = document.getElementById("menuId2");
+const btn4 = document.getElementById("menuId3");
 const menu = document.getElementById("menu");
 
 btn.addEventListener("click", () => {
-    const estado = menu.style.display;
+    const estado = window.getComputedStyle(menu).display;
     menu.style.display = estado === "none" ? "block" : "none";
-
     btn.src = estado === "none" ? "img/Close.png" : "img/Menu.png";
 });
+
+btn2.addEventListener("click", () => {
+    const estado = window.getComputedStyle(menu).display;
+    menu.style.display = "none";
+    btn.src = estado === "none" ? "img/Close.png" : "img/Menu.png";
+});
+
+btn3.addEventListener("click", () => {
+    const estado = window.getComputedStyle(menu).display;
+    menu.style.display = "none";
+    btn.src = estado === "none" ? "img/Close.png" : "img/Menu.png";
+});
+
+btn4.addEventListener("click", () => {
+    const estado = window.getComputedStyle(menu).display;
+    menu.style.display = "none";
+    btn.src = estado === "none" ? "img/Close.png" : "img/Menu.png";
+});
+
 
 particlesJS("particles-js", {
     "particles": {
